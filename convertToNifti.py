8a5f7c4de81e968bfda22d81bec780db65bf46b7
cosmo <me>
2017-04-01 21:53:05 -0700

    gradcam time

--- convertToNifti.py
+++ convertToNifti.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import nibabel
 import numpy as np # linear algebra
 from skimage import measure
-
+import numpy
 from viz import showHist, mayaPlot
 
 from utils import resample
@@ -34,7 +34,7 @@ from dsbTests import getImage
 image, imgNum = getImage(array, row)
 
 def vol2Nifti(vol, filename):
-	affine = tables.numpy.eye(4)
+	affine = numpy.eye(4)
 	#oImg = nibabel.Nifti1Image(image, affine, header=img.header)
 	oImg = nibabel.Nifti1Image(vol, affine)
 	oImg.to_filename(filename)

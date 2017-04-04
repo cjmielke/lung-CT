aeabc32f1193bad5100c79b0e2dc74e4a3e10839
cosmo <me>
2017-04-03 19:25:59 -0700

    sparse image builder

--- dsbTests.py
+++ dsbTests.py
@@ -187,8 +187,8 @@ class SparseImageSource():
 		self.cubeSize = self.array[0].shape[0]
 
 	def getImageFromSparse(self, row, convertType=True):
-		shape = row[['shapeZ', 'shapeY', 'shapeX']].as_matrix()
-		image = numpy.zeros(shape)
+		Z, Y, Z = row[['shapeZ', 'shapeY', 'shapeX']].as_matrix()
+		image = numpy.zeros((Z, Y, X, 1))
 
 		imageCubes = self.DF[self.DF.imgNum==row['imgNum']]
 		cs = self.cubeSize
@@ -196,7 +196,7 @@ class SparseImageSource():
 			z, y, z = cubeRow[['realZ', 'realY', 'realX']].as_matrix()
 			print z, y, x
 			cubeNum = cubeRow['cubeNum']
-			image[z:z+cs, y:y+cs, x:x+cs] = self.array[cubeNum] 
+			image[z:z+cs, y:y+cs, x:x+cs, :] = self.array[cubeNum] 
 
 		if convertType:
 			if image.dtype != K.floatx(): image = image.astype(K.floatx())
